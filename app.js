14e7b49ca5238506b913522c540688bed1daede3
Sambhav-gg <156508582+Sambhav-gg@users.noreply.github.com>
2026-06-05 17:13:43 +0530

    test3

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/api/status', (req, res) => {
             { name: 'Auto Scaling', status: 'operational' },
             { name: 'CloudWatch', status: 'operational' },
         ],
-        //to test push trigger
+        //to test push trigger for jenkins
         system: {
             uptime_seconds: uptime,
             uptime_human: formatUptime(uptime),
